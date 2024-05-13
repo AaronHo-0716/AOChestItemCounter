57ae64d75dbfc9aae6a747e9d8396e21dae85fc3
Aaron Ho <hoyanxun@gmail.com>
2024-05-13 10:40:47 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+function App() {
+
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+        Init
+      </div>
+    </>
+  )
+}
+
+export default App
