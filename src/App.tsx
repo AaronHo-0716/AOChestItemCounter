4698655a171e1e22ae0b0923212db96981db7499
Aaron Ho <hoyanxun@gmail.com>
2024-05-13 20:38:00 +0800

    kekw

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import './index.css'
+import { chestLogParser } from './aoChestLogParser'
+import { useState } from 'react'
 function App() {
+
+  const [chestLog, setChestLog] = useState<string>('')
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setChestLog(e.target.value)
+  }
 
   return (
     <>
-      <div className="flex h-screen justify-center items-center">
-        Init
+      <div className="flex flex-col h-screen justify-center items-center gap-2">
+        <h1 className="text-3xl">AOChestItemCounter</h1>
+        <input className="text-xl border border-black p-4" placeholder='Paste the chest logs here...' onChange={handleChange} />
+        <button className="border border-black p-4" onClick={() => chestLogParser(chestLog)}>Submit</button>
       </div>
     </>
   )
---
+++ src/aoChestLogParser.ts
@@ -0,0 +1,5 @@
+export const chestLogParser = (str: string): string[] => {
+  let arr = str.split(' ')
+  console.log(arr)
+  return arr
+}
