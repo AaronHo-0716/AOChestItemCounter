50e77915d1fcd90db16221ae5c6c04ec578d054e
Aaron Ho <hoyanxun@gmail.com>
2024-05-14 17:53:05 +0800

    idk why vite like this

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://aaronho-0716.github.io/AOChestItemCounter",
   plugins: [react()],
 })
